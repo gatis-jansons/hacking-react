94a2a41820dabe20075cdac7e39a7b130ee61b60
Bradley Bossard <bradleybossard@gmail.com>
2016-10-18 14:40:59 +0000

    Return multiple elements

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,13 +5,18 @@ class Detail extends React.Component {
   render() {
     //return <p>{this.props.message}</p>;
     //return <p>Hello {chance.first()}!</p>;
-    return <p>
+    return (<div>
+             <p>Hello {chance.first()}!</p>
+						<p>You're from {chance.country({ full: true })}.</p>
+					  </div>);
+/*
     {
         chance.first() == 'John'
         ? 'Hello, John!'
         : 'Hello, world!'
     }
     </p>;
+*/
   }
 }
 
