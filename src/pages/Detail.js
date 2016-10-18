0391b378e8f29a42a9be7bae0c321aa2b105159a
Bradley Bossard <bradleybossard@gmail.com>
2016-10-18 13:31:01 +0000

    Use chance to generate hello message

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
   render() {
-    return <p>{this.props.message}</p>;
+    //return <p>{this.props.message}</p>;
+    return <p>Hello {chance.first()}!</p>;
   }
 }
 
