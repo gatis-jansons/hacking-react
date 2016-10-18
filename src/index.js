d8f1b80f84ee3b24f02b1c90a313940aae083df2
Bradley Bossard <bradleybossard@gmail.com>
2016-10-18 13:24:06 +0000

    Refactor to use props

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import ReactDOM from 'react-dom';
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-  <Detail />,
+  <Detail message="This is coming from props!" />,
   document.getElementById('app')
 );
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Detail extends React.Component {
-    render() {
-        return <p>This is React rendering HTML!</p>;
-    }
+  render() {
+    return <p>{this.props.message}</p>;
+  }
 }
 
 export default Detail;
