5a90d3a0ef44f9d85d7068b55468d8296b7fb13f
Bradley Bossard <bradleybossard@gmail.com>
2016-10-18 13:42:59 +0000

    Add ternary

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -4,7 +4,14 @@ import Chance from 'chance';
 class Detail extends React.Component {
   render() {
     //return <p>{this.props.message}</p>;
-    return <p>Hello {chance.first()}!</p>;
+    //return <p>Hello {chance.first()}!</p>;
+    return <p>
+    {
+        chance.first() == 'John'
+        ? 'Hello, John!'
+        : 'Hello, world!'
+    }
+    </p>;
   }
 }
 
