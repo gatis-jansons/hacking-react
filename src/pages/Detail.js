b0800369426d9609d2ef4c90bd1e48495fb98f8b
Bradley Bossard <bradleybossard@gmail.com>
2016-10-19 02:50:04 +0000

    Clean out dead code

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,28 +1,8 @@
 import React from 'react';
-import Chance from 'chance';
 
 class Detail extends React.Component {
 	constructor(props) {
 		super(props);
-
-		const people = [];
-
-		for (let i = 0; i < 10; i++) {
-			people.push({
-				name: chance.first(),
-				country: chance.country({ full: true })
-			});
-		}
-
-		this.state = { people };
-	}
-
-	buttonClicked() {
-			const newState = {
-					name: chance.first()
-			};
-
-			this.setState(newState);
 	}
 
 	render() {
