b4aa0570925e4766c99cc5fd46f1518f641e8b96
Bradley Bossard <bradleybossard@gmail.com>
2016-10-19 02:51:50 +0000

    Added blank commits data

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,12 +3,14 @@ import React from 'react';
 class Detail extends React.Component {
 	constructor(props) {
 		super(props);
+
+    this.state = { commits: [] };
 	}
 
 	render() {
 		return (<div>
-				{this.state.people.map((person, index) => (
-							<p key={index}>Hello, {person.name} from {person.country}!</p>
+				{this.state.commits.map((commit, index) => (
+							<p key={index}>Some commit data here.</p>
 							))}
 				</div>);
 	}
