f87baf7e5e96bc33d42ea71cf1d48598ccf761e6
Bradley Bossard <bradleybossard@gmail.com>
2016-10-23 22:13:29 +0000

    Intro IndexRoute

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
+import App from './pages/App';
 import Detail from './pages/Detail';
 import List from './pages/List';
 
 ReactDOM.render(
-	<Router history={hashHistory}>
-	<Route path="/" component={ List } />
-	<Route path="/detail/:repo" component={ Detail } />
+	<Router history={hashHistory} >
+	  <Route path="/" component={ App }>
+		  <IndexRoute component={ List } />
+			<Route path="detail/:repo" component={ Detail } />
+    </Route>
 	</Router>,
 	document.getElementById('app')
 );
